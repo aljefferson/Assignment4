9d2108cd33c94c8e095404715f795ac011386022
aljefferson <aljefferson99501@gmail.com>
2014-05-02 18:10:41 -0700

    First upload
    Backup but still has bugs

---
+++ App_Code/ManageCustomer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for ManageCustomer
+/// </summary>
+public class ManageCustomer
+
+{
+    SqlConnection connect;
+
+    public ManageCustomer()
+    {
+        connect = new SqlConnection
+            (ConfigurationManager.ConnectionStrings["AutomartConnectionString"].ConnectionString);
+    }
+
+    public void WriteCustomer(Customer c)
+    {
+        string sqlPerson = "Insert into Person (LastName, Firstname) Values(@LastName, @FirstName)";
+        string sqlVehicle = " Insert into Customer.Vehicle(LicenseNumber, VehicleMake, VehicleYear, PersonKey) " +
+            "Values(@License, @Make, @Year, ident_Current('Person'))";
+        string sqlRegisteredCustomer =
+            "Insert into Customer.RegisteredCustomer(Email,CustomerPasscode, "
+        + "CustomerPassword, CustomerHashedPassword, PersonKey) "
+        + "Values(@email, @Passcode, @password, @hashedpass, ident_Current('Person'))";
+
+        SqlCommand personCmd = new SqlCommand(sqlPerson, connect);
+        personCmd.Parameters.AddWithValue("@LastName", c.LastName);
+        personCmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+
+        SqlCommand vehicleCmd = new SqlCommand(sqlVehicle, connect);
+        vehicleCmd.Parameters.AddWithValue("@License", c.LicenseNumber);
+        vehicleCmd.Parameters.AddWithValue("@Make", c.VehicleMake);
+        vehicleCmd.Parameters.AddWithValue("@Year", c.VehicleYear);
+
+        PasscodeGenerator pg = new PasscodeGenerator();
+        PasswordHash ph = new PasswordHash();
+        int passcode = pg.GetPasscode();
+        SqlCommand regCustomerCmd = new SqlCommand(sqlRegisteredCustomer, connect);
+        regCustomerCmd.Parameters.AddWithValue("@Email", c.Email);
+        regCustomerCmd.Parameters.AddWithValue("@Passcode", passcode);
+        regCustomerCmd.Parameters.AddWithValue("@password", c.PlainPassword);
+        regCustomerCmd.Parameters.AddWithValue("@hashedPass", ph.HashIt(c.PlainPassword.ToString(), passcode.ToString()));
+
+        
+        connect.Open();
+        personCmd.ExecuteNonQuery();
+        vehicleCmd.ExecuteNonQuery();
+        regCustomerCmd.ExecuteNonQuery();
+       connect.Close(); 
+
+
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        Customer c = new Customer();
+        c.LastName = txtLastName.Text;
+        c.FirstName = txtFirstName.Text;
+        c.LicenseNumber = txtLicense.Text;
+        c.VehicleMake = txtMake.Text;
+        c.VehicleYear = txtYear.Text;
+        c.PlainPassword = txtPassword.Text;
+        c.Email = txtEmail.Text;
+
+        ManageCustomer mc = new ManageCustomer();
+        mc.WriteCustomer(c);
+
+        lblError.Text= "Thanks for registering";
+    }
+}
