c15e8d4f4340668361b26d2a9f96a5fde948049d
aljefferson <aljefferson99501@gmail.com>
2014-05-07 17:08:10 -0700

    Login
    This version has the login file and all associated files it takes to
    make this work

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,15 +16,23 @@ public partial class _Default : System.Web.UI.Page
         Customer c = new Customer();
         c.LastName = txtLastName.Text;
         c.FirstName = txtFirstName.Text;
+        c.Email = txtEmail.Text;
         c.LicenseNumber = txtLicense.Text;
         c.VehicleMake = txtMake.Text;
         c.VehicleYear = txtYear.Text;
         c.PlainPassword = txtPassword.Text;
-        c.Email = txtEmail.Text;
+      
 
         ManageCustomer mc = new ManageCustomer();
-        mc.WriteCustomer(c);
-
-        lblError.Text= "Thanks for registering";
+        try
+        {
+            mc.WriteCustomer(c);
+            lblError.Text = "Thanks for registering";
+        }
+        catch (Exception ex)
+        {
+            lblError.Text = ex.Message;
+        }
+        
     }
 }
